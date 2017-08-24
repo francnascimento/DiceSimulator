33e3c00487002ea38335c3c04b9ce164c697b2ea
Francisco <pedro.wired@gmail.com>
2017-08-24 16:46:27 -0300

    Beginning the interface

--- init.py
+++ init.py
@@ -3,23 +3,23 @@ Francisco Pedro do Nascimento
 DiceSimulator - Just a program that simulates a dice for RPG play
 '''
 
-from DiceSimulator import DiceSimulator
+#from DiceSimulator import DiceSimulator
 
-valid_dices = ['D4','D6','D8','D12','D20']
+valid_dices = ["D4","D6","D8","D12","D20", "EXIT", "QUIT"]
 
 def selectSidesDice():
     index = -1
 
     while index < 0:
-        dice = input('Which dice do you want to roll? ')
+        dice = raw_input('Which dice do you want to roll? ')
         dice = dice.upper()
-        try:
-            index = valid_dices.index(dice)
-        except ValueError:
-            print('I\'m sorry is just avaible the options d4, d6, d8, d12 or d20')
+        if dice in valid_dices: index = valid_dices.index(dice)
+        else: print('I\'m sorry is just avaible the options d4, d6, d8, d12 or d20')
 
     sides = 0
-    if index == 0:
+    if index > 4:
+        exit()
+    elif index == 0:
         sides = 4
     elif index == 1:
         sides = 6
